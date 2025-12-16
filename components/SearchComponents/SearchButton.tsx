caa9411b4f28660c3857650d9468ad857b6bafc8
ORazB <devandramahottama@gmail.com>
2025-12-16 12:50:20 +0800

    Update Comment To Make It Cleaner

--- components/SearchComponents/SearchButton.tsx
+++ components/SearchComponents/SearchButton.tsx
@@ -1,12 +1,10 @@
 'use client';
-// UseState
-import { useState } from "react";
-
 // React
-import Link from "next/link";
+import { useState } from "react";
 
 // Next
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export default function Searchbutton() {
 
@@ -22,7 +20,7 @@ export default function Searchbutton() {
 
   return (
     <div className="w-full">
-      <input onKeyDown={handleKeyDown} onChange={(text) => setSearchQuery(text.target.value)} id="searchQuery" name="searchQuery" className="p-4 w-full rounded-full border border-gray-text focus:outline-transparent! focus:outline!" type="text" placeholder="Search..."></input>
+      <input onKeyDown={handleKeyDown} onChange={(e) => setSearchQuery(e.target.value)} id="searchQuery" name="searchQuery" className="p-4 w-full rounded-full border border-gray-text focus:outline-transparent! focus:outline!" type="text" placeholder="Search..."></input>
 
       <Link aria-disabled={!searchQuery} href={searchQuery ? `/courses/search/${searchQuery}` : '#'} className="flex items-center justify-center p-4 bg-primary rounded-full absolute top-0 right-0 h-full">
         <button className="text-white"><i className="cursor-pointer text-2xl bx bx-search"></i></button>

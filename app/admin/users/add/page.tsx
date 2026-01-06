86b881f3d087baaf12822545e1c961bd434c54a7
ORazB <devandramahottama@gmail.com>
2026-01-06 21:46:47 +0800

    Add admin panel for users table

---
+++ app/admin/users/add/page.tsx
@@ -0,0 +1,18 @@
+import { clerkClient } from "@clerk/nextjs/server";
+
+// Components
+import UsersAddPanel from "@/components/AdminComponents/Users/UsersAddPanel";
+
+export default function AddUserAdmin() {
+  return(
+  <section className="m-24">
+    <div className="container mx-auto max-w-2xl px-8">
+      <h1 className="text-text mb-8 text-4xl font-semibold tracking-wide">
+        Add User
+      </h1>
+
+      <UsersAddPanel />
+    </div>
+  </section>
+  )
+}

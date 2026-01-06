bb097a843f1fa74bdc5cc65f9b3005fd277535d3
ORazB <devandramahottama@gmail.com>
2026-01-06 21:42:09 +0800

    Add ClerkMiddleware so that regular users cannot access admin panel

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,26 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import prisma from "./lib/prisma";
+import { NextResponse } from 'next/server';
+import { UserRole } from "@/types/roles";
 
-export default clerkMiddleware();
+const isAdminRoute = createRouteMatcher(['/admin(.*)']);
+
+export default clerkMiddleware(async (auth, req) => {
+  const { userId } = await auth();
+  if (isAdminRoute(req)) {
+
+    if (!userId) return new NextResponse('Unauthorized', { status: 401 });
+    
+    const user = await prisma.user.findUnique({
+      where: { clerkId: userId || "" },
+      select: { role: true }
+    });
+
+    if (user?.role !== UserRole.ADMIN) {
+      return new NextResponse('Forbidden', { status: 403 });
+    }
+  }
+});
 
 export const config = {
   matcher: ['/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',

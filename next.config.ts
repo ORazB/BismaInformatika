4d2ca3f7a7393ad2d576ede3dd54f4cc651d438d
ORazB <devandramahottama@gmail.com>
2026-01-07 21:57:04 +0800

    Update NextConfig images.domains to images.remotePatterns because images.domains is deprecated

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['img.clerk.com'],
+    remotePatterns: [
+      {
+        hostname: 'img.clerk.com'
+      }
+    ]
+    // domains: ['img.clerk.com'],
   },
 };
 

093cb731977eae858bb7bc63bdc0e2dfc21c187d
ORazB <devandramahottama@gmail.com>
2026-01-08 19:21:36 +0800

    Add safety to delete user request

--- app/api/profile/delete/route.ts
+++ app/api/profile/delete/route.ts
@@ -1,7 +1,7 @@
 import prisma from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
-import { auth, clerkClient} from "@clerk/nextjs/server";
+import { auth, clerkClient } from "@clerk/nextjs/server";
 
 export async function DELETE(req: NextRequest) {
   try {
@@ -17,12 +17,20 @@ export async function DELETE(req: NextRequest) {
       return new NextResponse("Missing ID", { status: 400 })
     }
 
+    const actingUser = await prisma.user.findUnique({
+      where: { clerkId: userId }
+    })
+
+    if (actingUser?.role !== "ADMIN") {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
     const targetUser = await prisma.user.findUnique({
-      where: {id: parseInt(id)}
+      where: { id: parseInt(id) }
     })
 
     if (!targetUser) {
-      return NextResponse.json({ message: `Cannot find ClerkId from userId: ${id}`})
+      return NextResponse.json({ message: `Cannot find User from userId: ${id}` })
     }
 
     const client = await clerkClient();

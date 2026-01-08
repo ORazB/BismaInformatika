bb6f2d345aa34143b48e305ba9784c9b13fd074f
ORazB <devandramahottama@gmail.com>
2026-01-08 19:22:16 +0800

    Add courses delete request

---
+++ app/api/courses/delete/route.ts
@@ -0,0 +1,48 @@
+import prisma from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+import { clerkClient, auth } from "@clerk/nextjs/server";
+
+export async function DELETE(req: NextRequest) {
+  try {
+    const { userId } = await auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const id = req.nextUrl.searchParams.get("id");
+
+    if (!id) {
+      return new NextResponse("Missing ID", { status: 400 });
+    }
+
+    const actingUser = await prisma.user.findUnique({
+      where: { clerkId: userId }
+    })
+
+    if (actingUser?.role !== "ADMIN") {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const targetUser = await prisma.course.findUnique({
+      where: { id: parseInt(id) }
+    })
+
+    if (!targetUser) {
+      return NextResponse.json({ message: `Cannot find Course from id: ${id}` })
+    }
+
+    const deletedUser = await prisma.course.delete({
+      where: { id: parseInt(id) }
+    })
+
+    return NextResponse.json(deletedUser);
+  } catch (error) {
+    console.error("DELETE_ERROR: ", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}

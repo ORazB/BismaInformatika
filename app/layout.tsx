8435ac8b2c66d98571fe064261e7fa3056f993b1
Yogiksigma <daniwarmawan@gmail.com>
2026-01-08 19:26:14 -0800

    push

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,10 @@ import {
   SignedIn,
 } from "@clerk/nextjs";
 
+import { auth } from "@clerk/nextjs/server";
+import prisma from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -47,11 +51,29 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  async function getUserId() {
+    const { userId } = await auth();
+
+    const user = await prisma.user.findUnique({
+      where: { clerkId: String(userId) },
+    });
+
+    if (!user) {
+      return NextResponse.json(
+        { message: `Cannot find user on by clerkId: ${userId}` },
+        { status: 401 }
+      );
+    }
+    return user;
+  }
+
+  const user = getUserId();
+
   return (
     <ClerkProvider>
       <html lang="en">
         <body className={`${inter.className} antialiased`}>
-          <Navbar />
+          <Navbar actingUser={user} />
           {children}
         </body>
       </html>
@@ -59,6 +81,62 @@ export default function RootLayout({
   );
 }
 
-const Navbar = () => {
-  return <div></div>;
+const Navbar = (actingUser: any) => {
+  return (
+    <div className="w-full border-b-2 shadow border-zinc-300 bg-background p-2 z-10000 top-0 sticky">
+      <header className="container mx-auto flex justify-between w-4/5 items-center gap-4 h-16">
+        <nav className="flex items-center gap-8">
+          <Link href="/" className="logo">
+            <img src="/logos/logo-white.png"></img>
+          </Link>
+          <ul className="flex gap-10">
+            <li>
+              <a className="text-lg hover:underline" href="/">
+                Home
+              </a>
+            </li>
+            <li>
+              <a className="text-lg hover:underline" href="/courses">
+                Courses
+              </a>
+            </li>
+            <li>
+              <a className="text-lg hover:underline" href="/about">
+                About
+              </a>
+            </li>
+            <li>
+              <a className="text-lg hover:underline" href="/contact">
+                Contact Us
+              </a>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="clerk relative">
+          <SignedOut>
+            <SignInButton>
+              <button className="px-4 py-2 rounded-lg font-semibold hover:underline cursor-pointer">
+                Login
+              </button>
+            </SignInButton>
+
+            <div className="line absolute w-0.5 bottom-0 right-22 h-3/4 bg-black"></div>
+
+            <SignUpButton>
+              <button className="px-4 py-2 rounded-lg font-semibold hover:underline cursor-pointer">
+                Sign Up
+              </button>
+            </SignUpButton>
+          </SignedOut>
+
+          <SignedIn>
+            <div className="scale-125 cursor-pointer">
+              <UserProfileButton user={actingUser} />
+            </div>
+          </SignedIn>
+        </div>
+      </header>
+    </div>
+  );
 };

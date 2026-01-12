4797eed8ea5a53bfa0ec40f5158adf69869a30c8
ORazB <devandramahottama@gmail.com>
2026-01-12 16:08:42 +0800

    Add Categories props

--- app/admin/courses/page.tsx
+++ app/admin/courses/page.tsx
@@ -8,33 +8,26 @@ import CoursesViewPanel from "@/components/AdminComponents/courses/CoursesViewPa
 export default async function CourseAdminPage() {
 
   const allCourses = await prisma.course.findMany();
-  const client = await clerkClient();
+  const allCategories = await prisma.category.findMany();
 
   const coursesWithImages = await Promise.all(
     allCourses.map(async (course) => {
-      let image = course.imageUuid;
-
-      if (!image) {
-        console.log("imageURL on course doesn't exist (null)");
-      }
-
       return {
-        id: course.id,
-        title: course.title || "",
-        description: course.description || "",
-        imageUuid: image,
-        startDate: course.startDate || new Date(),
-        endDate: course.endDate || new Date(),
-        price: course.price ? course.price.toNumber() : 0,
-      }
-
+        ...course,
+        title: course.title ?? "",
+        description: course.description ?? "",
+        startDate: course.startDate, 
+        endDate: course.endDate,
+        price: course.price.toNumber(),
+        imageUuid: course.imageUuid 
+      };
     })
   )
 
   return (
     <div className="w-full">
       <div className="m-24">
-        <CoursesViewPanel validCourseData={coursesWithImages}/>
+        <CoursesViewPanel validCourseData={coursesWithImages} categories={allCategories}/>
       </div>
     </div>
   )
